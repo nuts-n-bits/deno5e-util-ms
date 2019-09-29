118c03b9435c2c2f7a6020b2fef4607e77097277
Xiaoyu <xiaoyusama@outlook.com>
2019-09-29 11:11:42 -0400

    build script changed

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 dist_dir="./dist"
 target_dir="../node_modules/lib-nnbc"
 
-echo "rm -rf" $dist_dir $target_dir
-rm -fr $dist_dir $target_dir
+echo "rm -rf" $dist_dir
+rm -fr $dist_dir
 
 echo "tsc"
 tsc
+
+echo "rm -rf" $target_dir
+rm -rf $target_dir
 
 echo "mv" $dist_dir $target_dir
 mv $dist_dir $target_dir
