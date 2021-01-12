508c6ed43286a57768237eea2a3c15960fa94fcc
nuts-n-bits <nuts-n-bits@outlook.com>
2021-01-12 17:19:02 -0500

    =

--- src/protocols/assert-passthrough.ts
+++ src/protocols/assert-passthrough.ts
@@ -8,66 +8,66 @@ export class AssertionNotNullOrUndefinedError extends Error {}
 export class AssertionNeverError extends Error {}
 
 // Verify functions is for compile time type hinting. It's for TS to throw TS errors and will never throw a JS error
-export function type_check<T>(value : T) : T {
+export function type_check<T>(value: T): T {
     return value
 }
 
-export function type_check_never(value : never): never
-export function type_check_never(value : never): void { return }
+export function type_check_never(value: never): never
+export function type_check_never(value: never): void { return }
 
 // Assert functions care just as much about compile time type correctness, but it will also throw JS error should runtime type check fails.
-export function assert_typeof(value : string, type : "string", comment? : string) : string
-export function assert_typeof(value : number, type : "number", comment? : string) : number
-export function assert_typeof(value : bigint, type : "bigint", comment? : string) : bigint
-export function assert_typeof(value : bigint, type : "symbol", comment? : string) : bigint
-export function assert_typeof(value : undefined, type : "undefined", comment? : string) : undefined
-export function assert_typeof(value : Function, type : "function", comment? : string) : Function
-export function assert_typeof(value : boolean, type : "boolean", comment? : string) : boolean
-export function assert_typeof(value : any, type : any, comment? : string) {
+export function assert_typeof(value: string, type: "string", comment?: string): string
+export function assert_typeof(value: number, type: "number", comment?: string): number
+export function assert_typeof(value: bigint, type: "bigint", comment?: string): bigint
+export function assert_typeof(value: bigint, type: "symbol", comment?: string): bigint
+export function assert_typeof(value: undefined, type: "undefined", comment?: string): undefined
+export function assert_typeof(value: Function, type: "function", comment?: string): Function
+export function assert_typeof(value: boolean, type: "boolean", comment?: string): boolean
+export function assert_typeof(value: any, type: any, comment?: string) {
     if(typeof value === type) return value
     else throw new AssertionTypeOfError(comment)
 }
 
-export function assert_constructor<T extends new (...args:any[]) => any>(item : any, constructor : T, comment? : string) : InstanceType<T> {
-    if(item instanceof constructor) return item
+export function assert_constructor<T extends new (...args:any[]) => any>(item: InstanceType<T>, constructor: T, comment?: string): InstanceType<T> {
+    if(item as any instanceof constructor) return item
     else throw new AssertionSameConstructorError(comment)
 }
 
-export function assert_truthy<T>(value : T, comment? : string) : T {
+export function assert_truthy<T>(value: T, comment?: string): T {
     if(value) return value
     else throw new AssertionTruthyError(comment)
 }
 
-export function assert_falsy<T>(value : T, comment? : string) : T {
+export function assert_falsy<T>(value: T, comment?: string): T {
     if(!value) return value
     else throw new AssertionFalsyError(comment)
 }
 
-export function assert_strict_equality<T>(lhs : T, rhs : T, comment? : string) : T {
+export function assert_strict_equality<T>(lhs: T, rhs: T, comment?: string): T {
     if(lhs === rhs) return lhs
     else throw new AssertionStrictEqualityError(comment)
 }
 
-export function assert_not_null<T>(value : T|null, comment? : string) : (T extends null ? never : T)
-export function assert_not_null<T>(value : T|null|undefined, comment? : string) : (T extends null ? never : T)|undefined
-export function assert_not_null(value : any, comment? : any) {
+export function assert_not_null<T>(value: T|null, comment?: string): (T extends null ? never : T)
+export function assert_not_null<T>(value: T|null|undefined, comment?: string): (T extends null ? never : T)|undefined
+export function assert_not_null(value: any, comment?: any) {
     if(value === null) throw new AssertionNotNullError(comment)
     else { return value }
 }
 
-export function assert_not_undefined<T>(value : T|undefined, comment? : string) : (T extends undefined ? never : T)
-export function assert_not_undefined<T>(value : T|null|undefined, comment? : string) : (T extends undefined ? never : T)|null
-export function assert_not_undefined(value : any, comment? : string) {
+export function assert_not_undefined<T>(value: T|undefined, comment?: string): (T extends undefined ? never: T)
+export function assert_not_undefined<T>(value: T|null|undefined, comment?: string): (T extends undefined ? never: T)|null
+export function assert_not_undefined(value: any, comment?: string) {
     if(value === undefined) throw new AssertionNotNullOrUndefinedError(comment)
     else { return value }
 }
 
-export function assert_not_null_or_undefined<T>(value : T, comment? : string) : NonNullable<T> {
+export function assert_not_null_or_undefined<T>(value: T, comment?: string): NonNullable<T> {
     if(value === null || value === undefined) { throw new AssertionNotNullOrUndefinedError(comment) }
     else { return value! }
 }
 
-export function assert_never(value : never, comment? : string) : never {
+export function assert_never(value: never, comment?: string): never {
     throw new AssertionNeverError(comment)
 }
 
